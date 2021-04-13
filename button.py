bc142b03c60bc91a028a7285335eff3f088035fc
keombre <keombre8@gmail.com>
2021-04-13 20:38:46 +0100

    direct db access

--- button.py
+++ button.py
@@ -1,14 +1,17 @@
 from gpiozero import Button
-from urllib import request
 from datetime import datetime
+import select
+import sqlite3
 
 def released():
     print("opened " + str(datetime.now()))
-    request.urlopen(request.Request("http://127.0.0.1/add", data=b''))
+    con = sqlite3.connect("./db.sqlite")
+    con.cursor().execute("INSERT INTO entries VALUES (?, 0);", (datetime.now(),))
+    con.commit()
+    con.close()
 
 btn = Button(26, pull_up=True, bounce_time=0.2, hold_time=0.5)
 btn.when_released = released
 
-while True:
-    input()
+select.select([], [], [])
 
--- db.py
+++ db.py
@@ -10,29 +10,39 @@ class DB:
         con = cls.get_db()
         con.cursor().execute('CREATE TABLE IF NOT EXISTS entries (date INTEGER NOT NULL, read INTEGER NOT NULL DEFAULT 0);')
         con.commit()
+        con.close()
 
     @classmethod
     def add_entry(cls, date):
         con = cls.get_db()
         con.cursor().execute('INSERT INTO entries VALUES (?, 0);', (date,))
         con.commit()
+        con.close()
 
     @classmethod
     def get_entries(cls, offset = 0, paginate_size = 10):
-        return cls.get_db().cursor().execute('SELECT rowid, * FROM entries ORDER BY date DESC LIMIT ? OFFSET ?;', (paginate_size, paginate_size * offset)).fetchall()
+        con = cls.get_db()
+        ret = con.cursor().execute('SELECT rowid, * FROM entries ORDER BY date DESC LIMIT ? OFFSET ?;', (paginate_size, paginate_size * offset)).fetchall()
+        con.close()
+        return ret
 
     @classmethod
     def count_entries(cls):
-        return cls.get_db().cursor().execute('SELECT COUNT(*) FROM entries;').fetchone()
+        con = cls.get_db()
+        ret = con.cursor().execute('SELECT COUNT(*) FROM entries;').fetchone()
+        con.close()
+        return ret
 
     @classmethod
     def mark_read(cls, id):
         con = cls.get_db()
         con.cursor().execute('UPDATE entries SET read = 1 WHERE rowid = ?;', (id,))
         con.commit()
+        con.close()
 
     @classmethod
     def mark_unread(cls, id):
         con = cls.get_db()
         con.cursor().execute('UPDATE entries SET read = 0 WHERE rowid = ?;', (id,))
         con.commit()
+        con.close()
