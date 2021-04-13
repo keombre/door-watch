d2358f35c16b8a7b243ca0f8f5bbee8a5980a1db
keombre <keombre8@gmail.com>
2021-04-13 20:00:28 +0100

    fix button

--- button.py
+++ button.py
@@ -1,8 +1,14 @@
 from gpiozero import Button
-from urllib2 import request
+from urllib import request
+from datetime import datetime
 
-btn = Button(26, pull_up=True, bounce_time=0.2)
+def released():
+    print("opened " + str(datetime.now()))
+    request.urlopen(request.Request("http://127.0.0.1/add", data=b''))
+
+btn = Button(26, pull_up=True, bounce_time=0.2, hold_time=0.5)
+btn.when_released = released
 
 while True:
-    btn.wait_for_press()
-    request.urlopen(request.Request("http://127.0.0.1/add", data=urllib.urlencode({}))
+    input()
+
